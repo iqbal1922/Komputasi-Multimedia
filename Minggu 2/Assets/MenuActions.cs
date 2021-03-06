63460869b4a832505e95be55de7961391f2b5626
Iqbaludin <muhammadiqbaluddin22@gmail.com>
2021-03-07 02:09:48 +0700

    Update

---
+++ Minggu 2/Assets/MenuActions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuActions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void MENU_ACTION_GotoPage(string sceneName){
+        Application.LoadLevel(sceneName);
+    }
+
+}
