376ffc19e13decfa33824e6b17e941dad8580ca9
Erninda <44139473+Erninda@users.noreply.github.com>
2021-05-09 18:52:03 +0700

    update

---
+++ Minggu 12/Project121/Assets/AudioObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AudioObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void Update(){
+        if (!audioSource.isPlaying)
+            Destroy(gameObject); 
+    }
+}
---
+++ Minggu 12/Project121/Assets/ButtonActions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActions: MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioObject audioDestructScriptedObject;
+    public void PlaySound(){
+       if (!audioSource.isPlaying)
+           audioSource.Play();
+    }
+    public void DestroyAfterSoundStops()
+    {
+       audioDestructScriptedObject.enabled = true;
+        
+    }
+}
