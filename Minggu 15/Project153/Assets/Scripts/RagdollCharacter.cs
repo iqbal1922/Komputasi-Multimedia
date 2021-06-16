48d536c6d36a79b5ebc57a4896ec5128223b9770
Erninda <44139473+Erninda@users.noreply.github.com>
2021-06-16 11:44:37 +0700

    udpate

---
+++ Minggu 15/Project153/Assets/Scripts/RagdollCharacter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RagdollCharacter : MonoBehaviour{
+    
+    void Start(){
+        DeactivateRagdoll();
+    }
+    public void ActivateRagdoll(){
+    gameObject.GetComponent<CharacterController> ().enabled = false;
+    gameObject.GetComponent<BasicController> ().enabled = false;
+    gameObject.GetComponent<Animator> ().enabled = false;
+    foreach (Rigidbody bone in GetComponentsInChildren<Rigidbody>()){
+          bone.isKinematic = false;
+          bone.detectCollisions = true;
+        }
+    foreach (Collider col in GetComponentsInChildren<Collider>()){
+        col.enabled = true;
+       }
+       StartCoroutine (Restore ());
+       }
+       public void DeactivateRagdoll(){
+           
+           gameObject.GetComponent<BasicController>().enabled = true;
+           gameObject.GetComponent<Animator>().enabled = true;
+           transform.position = GameObject.Find("Spawpoint").transform.position;
+           transform.rotation = GameObject.Find("Spawpoint").transform.rotation;
+           foreach(Rigidbody bone in GetComponentsInChildren<Rigidbody>()){
+                   bone.isKinematic = true;
+                     bone.detectCollisions = false;
+           }
+           foreach (CharacterJoint joint in GetComponentsInChildren<CharacterJoint>()){
+               joint.enableProjection = true;
+            }
+             foreach(Collider col in GetComponentsInChildren<Collider>()){
+               col.enabled = false;
+            }
+            gameObject.GetComponent<CharacterController>().enabled = true;
+           }
+
+           IEnumerator Restore(){
+            yield return new WaitForSeconds(5);
+            DeactivateRagdoll();
+    }
+}
